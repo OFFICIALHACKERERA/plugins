f97c0f10a1560082c42a010af9391aa5c2e6adff
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-07-10 15:45:38 +0530

    Add files via upload

---
+++ hellbot/plugins/ping.py
@@ -0,0 +1,28 @@
+import asyncio
+import datetime
+
+from . import *
+
+PING_PIC = Config.ALIVE_PIC
+
+@bot.on(admin_cmd(pattern="ping$", outgoing=True))
+@bot.on(sudo_cmd(pattern="ping$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.datetime.now()
+    event = await eor(event, "__**(❛ ⚡️ρσɳɠ⚡️ ❜!**")
+    end = datetime.datetime.now()
+    ms = (end - start).microseconds / 1000
+    if PING_PIC:
+        d3vil_caption = f"⚡️ρσɳɠ🦋**\n\n⚡️{ms}\n⚡**My** **Master**  {d3vil_mention}"
+        await event.client.send_file(
+            event.chat_id, PING_PIC, caption=d3vil_caption
+        )
+        await event.delete()
+
+CmdHelp("ping").add_command(
+  "ping", None, "Checks the ping speed of your 𝔇3𝔳𝔦𝔩𝔅𝔬𝔱"
+).add()
+
+
