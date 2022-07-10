dea0b42c3bc84157364466ba5cf6f242f7883d0a
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-07-10 16:22:09 +0530

    Update ping.py

--- hellbot/plugins/ping.py
+++ hellbot/plugins/ping.py
@@ -15,7 +15,7 @@ async def _(event):
     end = datetime.datetime.now()
     ms = (end - start).microseconds / 1000
     if PING_PIC:
-        d3vil_caption = f"⚡️ρσɳɠ🦋**\n\n⚡️{ms}\n⚡**My** **Master**  </i> {}"
+        d3vil_caption = f"⚡️ρσɳɠ🦋**\n\n⚡️{ms}\n⚡**My** **Master**"
         await event.client.send_file(
             event.chat_id, PING_PIC, caption=d3vil_caption
         )
