417e76f82235e1bcea35e04c5c59d91f5f658237
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-07-11 20:49:14 +0530

    Update raid.py

--- hellbot/plugins/raid.py
+++ hellbot/plugins/raid.py
@@ -414,8 +414,10 @@ CmdHelp("raid").add_command(
     "raid",
     "<Reply to a User With Number>",
     "Use and see",
-    ".raid 10 reply to anyone message",
+    "raid 10 reply to anyone message",
+
+).add_command("replyraid", None, "Reply to him or her to start raid").add_command(
+    "dreplyraid", None, "Reply To her Ya him To stop raid"
+    "Addons"
 ).add()
-
-
 
