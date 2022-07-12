8e0cabb30db250e0bbba6c1f62323cd4ae6290a1
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-07-12 09:44:25 +0530

    Update assistant.py

--- hellbot/plugins/assistant.py
+++ hellbot/plugins/assistant.py
@@ -7,9 +7,6 @@ from telethon import events
 from telethon.errors.rpcerrorlist import MessageDeleteForbiddenError
 from telethon.tl.types import ChannelParticipantsAdmins
 from userbot import bot
-from telethon import events
-from telethon.utils import pack_bot_file_id
-rom telethon import events
 from telethon.errors import BadRequestError
 from telethon.tl.functions.channels import EditAdminRequest, EditBannedRequest
 from telethon.tl.functions.messages import UpdatePinnedMessageRequest
