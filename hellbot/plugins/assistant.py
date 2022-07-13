666a8a65a1c64521db3122b7b69a4e1c191d6380
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-07-14 00:58:14 +0530

    Update assistant.py

--- hellbot/plugins/assistant.py
+++ hellbot/plugins/assistant.py
@@ -1,11 +1,11 @@
 from telethon import events
 
 PM_IMG = "https://telegra.ph/file/c26fc61e904476083baa7.jpg"
-pm_caption = f"⚜ LegendBot is Online ⚜ \n\n"
+pm_caption = f"⚜ Assistant is Online ⚜ \n\n"
 pm_caption += f"Owner ~ 『{legend_mention}』\n"
 pm_caption += f"**╭───────────**\n"
 pm_caption += f"┣Telethon ~ `1.15.0` \n"
-pm_caption += f"┣『LegendBot』~ `{LEGENDversion}` \n"
+pm_caption += f"┣『LegendBot』~ `2.0` \n"
 pm_caption += f"┣Channel ~ [Channel](https://t.me/LegendBot_AI)\n"
 pm_caption += f"┣**Licenece** ~ [Licence](https://github.com/ITS-LEGENDBOT/LEGENDBOT/blob/master/LICENSE)\n"
 pm_caption += f"┣Copyright ~ By [LegendBot』 ](https://t.me/LegendBot_OP)\n"
