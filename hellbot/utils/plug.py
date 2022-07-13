c4ec0d97f93185e3d0b2b930e85aca9dcc62c5e5
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-07-13 10:50:20 +0530

    Update plug.py

--- hellbot/utils/plug.py
+++ hellbot/utils/plug.py
@@ -40,7 +40,7 @@ def load_module(shortname):
         import hellbot.utils
 
         path = Path(f"hellbot/plugins/assistant/{shortname}.py")
-        name = "hellbot.plugins.{}".format(shortname)
+        name = "hellbot.plugins.assistant.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
@@ -48,7 +48,7 @@ def load_module(shortname):
     else:
         import hellbot.utils
         path = Path(f"hellbot/plugins/assistant/{shortname}.py")
-        name = "hellbot.plugins.{}".format(shortname)
+        name = "hellbot.plugins.assistant.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         mod.bot = Hell
