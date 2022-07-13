635e731edafec66dab4ec62ecc07e0d9fa197821
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-07-13 12:28:34 +0530

    Update __main__.py

--- hellbot/__main__.py
+++ hellbot/__main__.py
@@ -80,7 +80,7 @@ async def start_hellbot():
         LOGS.info("••• Starting to load Plugins •••")
         await plug_load("hellbot/plugins/*.py")
         await plug_channel(bot, Config.PLUGIN_CHANNEL)
-        LOGS.info("⚡ Your HellBot Is Now Working ⚡")
+        LOGS.info("⚡ Your UserBot.. Is Now Working ⚡")
         LOGS.info("Head to @Its_HellBot for Updates. Also join chat group to get help regarding to HellBot.")
         LOGS.info(f"» Total Clients = {str(total)} «")
         await hell_is_on(total)
