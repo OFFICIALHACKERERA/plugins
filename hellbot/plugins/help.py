5fb96904865a88f57aa80ad6efe3dd6978ab45f3
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-07-19 14:09:52 +0530

    Update help.py

--- hellbot/plugins/help.py
+++ hellbot/plugins/help.py
@@ -7,7 +7,7 @@ from ..core import CMD_INFO, GRP_INFO, PLG_INFO
 from ..core.managers import eod, eor
 from ..helpers.utils import reply_id
 
-cmdprefix = Config.HANDLER
+
 
 menu_category = "tools"
 
